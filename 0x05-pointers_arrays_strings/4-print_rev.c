6bc9b71a9f963ac398be84292553328b9985baad
Yahya Eddhissa <yahya.eddhissa@proton.me>
2023-09-18 02:58:21 -0700

    Print a string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse
+ * @s: string
+ * return: 0
+ */
+void print_rev(char *s)
+{
+	int length = 0;
+	int o;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	
+	s--;
+	
+	for (o = length; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+
+	_putchar('\n');
+}
