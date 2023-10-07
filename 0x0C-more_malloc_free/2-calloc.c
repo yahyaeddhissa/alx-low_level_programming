338525d8fba7e478d1cf6b1f225977b526a2243b
Yahya Eddhissa <yahya.eddhissa@proton.me>
2023-10-07 02:40:21 -0700

    Allocate memory for an array

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *_memfill - Fills memory with a constant byte
+ * @s: Memory area to be filled
+ * @b: Char to copy
+ * @n: Number of times to copy b
+ *
+ * Return: Pointer to the memory area s
+ */
+char *_memfill(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
+
+/**
+ * *_calloc - Allocates memory for an array
+ * @nmemb: Number of elements in the array
+ * @size: Size of each element
+ *
+ * Return: Pointer to allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(size * nmemb);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	_memfill(ptr, 0, nmemb * size);
+
+	return (ptr);
+}
