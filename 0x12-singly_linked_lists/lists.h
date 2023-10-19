078746b349d2ded43eaada1735e0437084ee4cd9
Yahya Eddhissa <yahya.eddhissa@proton.me>
2023-10-19 15:16:33 -0700

    Free a list from memory

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -8,7 +8,6 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
  */
 typedef struct list_s
 {
@@ -21,5 +20,6 @@ size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif
