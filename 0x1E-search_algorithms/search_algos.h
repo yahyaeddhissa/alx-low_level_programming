94940c7350b43c7eed3586f219a41c73b6387b2f
Yahya Eddhissa <yahya.eddhissa@proton.me>
2024-04-10 08:14:39 -0700

    Skip search on singly linked lists

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -12,6 +12,14 @@ typedef struct listint_s
 		struct listint_s *next;
 } listint_t;
 
+typedef struct skiplist_s
+{
+				int n;
+				size_t index;
+				struct skiplist_s *next;
+				struct skiplist_s *express;
+} skiplist_t;
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
@@ -19,5 +27,6 @@ int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 
 #endif /* SEARCH_ALGOS_H */
